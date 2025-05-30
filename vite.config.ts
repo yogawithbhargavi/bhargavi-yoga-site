0d1ee107fe0d4822a4deeec1206c4d15becb8d2a
yogawithbhargavi <yogawithbhargavi@gmail.com>
2025-05-30 18:35:25 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   server: {
-    port: 3001, 
+    host: '0.0.0.0',
+    port: 3001,
   },
-});
+})
